429fb2a649864d4518dba0b9ab44557c84e419fa
PedroOlivares77 <160944277+PedroOlivares77@users.noreply.github.com>
2025-12-14 21:30:58 +0100

    Use secure_asset for favicon and stylesheets

--- resources/views/baseAdmin.blade.php
+++ resources/views/baseAdmin.blade.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" href="{{ asset('assets/media/img/favicon.png') }}" type="image/png">
+    <link rel="icon" href="{{ secure_asset('assets/media/img/favicon.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/2.2.2/css/dataTables.dataTables.css" />
-    <link rel="stylesheet" type="text/css" href="{{asset ('assets/css/estilos.css') }}" />
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/estilos.css') }}">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
     <title>@yield('titulo')</title>
